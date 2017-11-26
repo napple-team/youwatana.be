ab4f283ab3aefd638779187d8932c8be04d229e4
windyakin <windyakin@gmail.com>
2017-11-26 20:40:54 +0900

    dotenv gemを入れる

--- api_server/config/application.rb
+++ api_server/config/application.rb
@@ -29,5 +29,11 @@ module ApiServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    if Rails.env.development? || Rails.env.test?
+      Dotenv.overload(
+        Rails.root.join(".env.#{Rails.env}"),
+      )
+    end
   end
 end
