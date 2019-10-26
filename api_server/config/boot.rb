a573530e2b371817438bc318c0fdbc0ededc9588
windyakin <windyakin@gmail.com>
2019-10-26 17:19:18 +0900

    STDOUT.sync = true on Docker

--- api_server/config/boot.rb
+++ api_server/config/boot.rb
@@ -2,3 +2,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+if ENV['STDOUT_SYNC'] == 'true'
+  STDOUT.sync = true
+end
