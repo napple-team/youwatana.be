b695f8efb143075c7e33dd3dbb2dd7591063bd17
windyakin <windyakin@gmail.com>
2019-10-27 17:34:27 +0900

    WebSocketの接続を管理する
    * 接続できていない場合にボタンを押せないようにする

--- frontend/components/counter.vue
+++ frontend/components/counter.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="text-center py-5">
-    <p class="display-3">{{ animatedCounter }}</p>
-    <button class="btn btn-outline-primary btn-lg" id="button-yosoro" @click=handleClick>(*&gt; &#7447; &bull;*)ゞ</button>
+    <template v-if=disconnected>
+      <div class="display-3">Loading...</div>
+      <button class="btn btn-outline-primary btn-lg my-3 px-5" disabled>
+        <span class="spinner-border" role="status" aria-hidden="true">
+          <span class="sr-only">Loading...</span>
+        </span>
+      </button>
+    </template>
+    <template v-else>
+      <div class="display-3">{{ animatedCounter }}</div>
+      <button class="btn btn-outline-primary btn-lg my-3" id="button-yosoro" @click=handleClick>
+        (*&gt; &#7447; &bull;*)ゞ
+      </button>
+    </template>
   </div>
 </template>
 
@@ -12,18 +24,21 @@ import { TweenLite } from 'gsap/TweenLite'
 export default {
   data() {
     return {
+      disconnected: true,
       count: 0,
       tweenedNumber: 0,
     }
   },
   created() {
     const channel = actioncable.createConsumer('ws://localhost:13000/cable')
-
     this.counterChannel = channel.subscriptions.create('CounterChannel', {
-      received: (data) => {
-        this.count = data['count']
-      }
+      connected: () => { this.disconnected = false },
+      rejected: () => { this.disconnected = true },
+      disconnected: (data) => { this.disconnected = true },
     })
+  },
+  beforeDestroy() {
+    this.counterChannel.unsubscribe()
   },
   computed: {
     animatedCounter: function() {
@@ -32,11 +47,11 @@ export default {
   },
   methods: {
     handleClick: function() {
-      this.counterChannel.perform('increment', {
+        this.counterChannel.perform('increment', {
         count: 1,
-      })
-    }
-  },
+        })
+      }
+    },
   watch: {
     count: function(newValue, oldValue){
       TweenLite.to(this.$data, 0.5, { tweenedNumber: newValue })
